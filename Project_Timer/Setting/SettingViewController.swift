6eeb76f517e67de17b685785105ed410f21f1c4d
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-05-29 01:35:33 +0900

    [Feat] Setting storyboard VC 생성 및 기본 UI 구현

---
+++ Project_Timer/Setting/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/05/28.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
