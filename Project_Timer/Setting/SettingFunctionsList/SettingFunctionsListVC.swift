fe8e743f474369c11809b41a88dce99ef9d4cced
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-06-04 10:00:25 +0900

    [Feat] PushVC 로직 구현 완료

---
+++ Project_Timer/Setting/SettingFunctionsList/SettingFunctionsListVC.swift
@@ -0,0 +1,31 @@
+//
+//  SettingFunctionsListVC.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/06/04.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+final class SettingFunctionsListVC: UIViewController {
+    static let identifier = "SettingFunctionsListVC"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
