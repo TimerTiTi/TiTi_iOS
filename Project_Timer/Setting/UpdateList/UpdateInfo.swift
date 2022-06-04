3ec6387a3e98c0e36668df9182ebfe7e9535f2db
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-06-04 11:43:28 +0900

    [Feat] updateList 기본로직 구현

---
+++ Project_Timer/Setting/UpdateList/UpdateInfo.swift
@@ -0,0 +1,15 @@
+//
+//  UpdateInfo.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/06/04.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import Foundation
+
+struct UpdateInfo {
+    let version: String
+    let date: String
+    let text: String
+}
