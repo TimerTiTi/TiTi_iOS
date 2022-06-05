ff8338b125e73771f483f67fa693146639062a57
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-06-05 10:57:59 +0900

    [Feat] TiTiFactory 기본UI 구성

---
+++ Project_Timer/Setting/SettingTiTiFactoryList/TiTiFactoryHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  TiTiFactoryHeaderView.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/06/05.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+final class TiTiFactoryHeaderView: UICollectionReusableView {
+    static let identifier = "TiTiFactoryHeaderView"
+        
+    @IBAction func showDevelopmentList(_ sender: Any) {
+        
+    }
+    @IBAction func goInstaToTiTi(_ sender: Any) {
+        
+    }
+    
+}
