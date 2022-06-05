eec17d500d104efb077c31726601320853cf341c
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-06-05 13:25:27 +0900

    [Feat] 사용방법 youtube 버튼 생성

--- Project_Timer/Network/NetworkURL.swift
+++ Project_Timer/Network/NetworkURL.swift
@@ -13,4 +13,5 @@ enum NetworkURL {
     static let appstore: String = "itms-apps://itunes.apple.com/app/id1519159240"
     static let developmentList: String = "https://deeply-eggplant-5ec.notion.site/TiTi-Development-List-b089afc1a4eb4cdb8c06840ca9cb1273"
     static let instagramToTiTi: String = "https://www.instagram.com/study_withtiti/"
+    static let youtubeLink: String = "https://youtu.be/VGbJT5zm9wE"
 }
