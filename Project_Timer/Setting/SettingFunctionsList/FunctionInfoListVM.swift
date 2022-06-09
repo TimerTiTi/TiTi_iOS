569a1b0707858eaa70836de8dbfab9448ca5b60c
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-06-09 11:41:56 +0900

    [Refactor] weak self 누락 수정, youtubeLink firestore 연결

--- Project_Timer/Setting/SettingFunctionsList/FunctionInfoListVM.swift
+++ Project_Timer/Setting/SettingFunctionsList/FunctionInfoListVM.swift
@@ -11,17 +11,29 @@ import Combine
 
 final class FunctionInfoListVM {
     @Published private(set) var infos: [FunctionInfo] = []
+    private(set) var link: String?
     
     init() {
         self.configureInfos()
+        self.configureYoutubeLink()
     }
     
     private func configureInfos() {
-        FirestoreManager.shared.db.collection("titifuncs").getDocuments { querySnapshot, error in
+        FirestoreManager.shared.db.collection("titifuncs").getDocuments { [weak self] querySnapshot, error in
             if let error = error {
                 print("Error getting documents: \(error)")
             } else {
-                self.infos = querySnapshot!.documents.map { FunctionInfo(data: $0.data()) }
+                self?.infos = querySnapshot!.documents.map { FunctionInfo(data: $0.data()) }
+            }
+        }
+    }
+    
+    private func configureYoutubeLink() {
+        FirestoreManager.shared.db.collection("links").document("youtube").getDocument { [weak self] snapshot, error in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                self?.link = snapshot?.data()?["url"] as? String
             }
         }
     }
