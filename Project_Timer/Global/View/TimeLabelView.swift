02b819695ff69bb1f55064619961faba7d8adf2e
Sujeong Choi <babeeboo2000@gmail.com>
2022-06-24 00:01:28 +0900

    [Feat] 시간표시 라벨뷰와 뷰모델 구현

---
+++ Project_Timer/Global/View/TimeLabelView.swift
@@ -0,0 +1,31 @@
+//
+//  TimeLabelView.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/06/23.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimeLabelView: View {
+    @ObservedObject var viewModel: TimeLabelViewModel
+    
+    var body: some View {
+        GeometryReader { geometry  in
+            VStack(spacing: 0) {
+                Group {
+                    Text("\(viewModel.oldValue)")
+                    Text("\(viewModel.newValue)")
+                }
+                .font(.system(size: 30))
+                .minimumScaleFactor(0.1)
+                .frame(width: geometry.size.width,
+                       height: geometry.size.height)
+            }
+            .offset(x: 0, y: viewModel.update ? -geometry.size.height : 0)
+            .frame(maxHeight: geometry.size.height, alignment: .top)
+            .clipped()
+        }
+    }
+}
