0bf51043685dfbd2287005bf15854092ac964c6d
S057_최수정 <babeeboo2000@gmail.com>
2022-06-24 17:45:37 +0900

    [Feat] TimeLabelView를 위한 모델 선언

--- Project_Timer/Global/Model/Times.swift
+++ Project_Timer/Global/Model/Times.swift
@@ -8,9 +8,42 @@
 
 import Foundation
 
+struct TimeLabel {
+    let hourTens: Int
+    let hourUnits: Int
+    let minuteTens: Int
+    let minuteUnits: Int
+    let secondTens: Int
+    let secondUnits: Int
+}
+
 struct Times {
     let sum: Int
     let timer: Int
     let stopwatch: Int
     let goal: Int
+    
+    static func hourFromSeconds(_ sec: Int) -> Int {
+        return sec / 3600
+    }
+    
+    static func minuteFromSeconds(_ sec: Int) -> Int {
+        return (sec - sec/3600*3600) / 60
+    }
+    
+    static func secondFromSeconds(_ sec: Int) -> Int {
+        return sec % 60
+    }
+    
+    static func toTimeLabel(_ sec: Int) -> TimeLabel {
+        let hour = hourFromSeconds(sec)
+        let minute = minuteFromSeconds(sec)
+        let second = secondFromSeconds(sec)
+        return TimeLabel(hourTens: hour / 10,
+                         hourUnits: hour % 10,
+                         minuteTens: minute / 10,
+                         minuteUnits: minute % 10,
+                         secondTens: second / 10,
+                         secondUnits: second % 10)
+    }
 }
