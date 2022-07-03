3334ed5d0864abc5d42e4df3dd9fbd4f99d335b2
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-03 15:51:01 +0900

    [Feat] StringValue Encodable 구조체 추가

---
+++ Project_Timer/Network/DTO/Decodable/StringValue.swift
@@ -0,0 +1,21 @@
+//
+//  StringValue.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/07/03.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import Foundation
+
+struct StringValue: Encodable {
+    let value: String
+    
+    init(value: String) {
+        self.value = value
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case value = "stringValue"
+    }
+}
