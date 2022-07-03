4a40f48e5fa7ec7b01147f6f529fa9878e018b0a
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-03 21:21:28 +0900

    [Refactor] AppDelegate 내 Firestore 관련 import 제거, AppDelegate 관련없는 로직 각 파일로 분리

---
+++ Project_Timer/Global/Extension/UserDefaults+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UserDefaults+Extension.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/07/03.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+extension UserDefaults {
+    func colorForKey(key: String) -> UIColor? {
+        var color: UIColor?
+        if let colorData = data(forKey: key) {
+            color = NSKeyedUnarchiver.unarchiveObject(with: colorData) as? UIColor
+        }
+        return color
+    }
+    
+    func setColor(color: UIColor?, forKey key: String) {
+        var colorData: NSData?
+        if let color = color {
+            colorData = NSKeyedArchiver.archivedData(withRootObject: color) as NSData?
+        }
+        set(colorData, forKey: key)
+    }
+}
