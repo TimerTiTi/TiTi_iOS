179b1d7b2547d87af7168fbcc149b7858dd5e210
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-04 13:03:47 +0900

    [Feat] TiTiLab localized 반영

--- Project_Timer/Setting/SettingTiTiLab/TiTiLabHeaderView.swift
+++ Project_Timer/Setting/SettingTiTiLab/TiTiLabHeaderView.swift
@@ -9,9 +9,24 @@
 import UIKit
 
 final class TiTiLabHeaderView: UICollectionReusableView {
+    /* public */
     static let identifier = "TiTiLabHeaderView"
+    /* private */
+    @IBOutlet weak var developmentStatusLabel: UILabel!
+    @IBOutlet weak var developmentListLabel: UILabel!
+    @IBOutlet weak var developmentListTextLabel: UILabel!
+    @IBOutlet weak var instagramTextLabel: UILabel!
+    
+    @IBOutlet weak var participationDevelopLabel: UILabel!
+    @IBOutlet weak var surveyLabel: UILabel!
+    @IBOutlet weak var surveyTextLabel: UILabel!
     
     private weak var delegate: TiTiLabActionDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.configureLocalized()
+    }
     
     func configure(delegate: TiTiLabActionDelegate) {
         self.delegate = delegate
@@ -23,5 +38,18 @@ final class TiTiLabHeaderView: UICollectionReusableView {
     
     @IBAction func goInstaToTiTi(_ sender: Any) {
         self.delegate?.showWebview(url: NetworkURL.instagramToTiTi)
+    }
+}
+
+extension TiTiLabHeaderView {
+    private func configureLocalized() {
+        self.developmentStatusLabel.text = "TiTi Development".localized()
+        self.developmentListLabel.text = "Development list".localized()
+        self.developmentListTextLabel.text = "Real-time development progress can be checked.\nYou can also check the next features in advance.".localized()
+        self.instagramTextLabel.text = "Check out the news on TiTi's official Instagram account.".localized()
+        
+        self.participationDevelopLabel.text = "Participation in Develop".localized()
+        self.surveyLabel.text = "Survey".localized()
+        self.surveyTextLabel.text = "It greatly helps to develop and improve new features.\nNew surveys are added in real time.".localized()
     }
 }
