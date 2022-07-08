9ebba387a0b5966be68c7171520dc4e8f7636aad
S057_최수정 <babeeboo2000@gmail.com>
2022-07-08 15:18:39 +0900

    [Feat] 카운트다운 TimeLabel 구현

---
+++ Project_Timer/Global/View/CountdownTimeLabelView.swift
@@ -0,0 +1,23 @@
+//
+//  CountDownTimeLabelView.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/07/08.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct CountdownTimeLabelView: View {
+    @ObservedObject var viewModel: CountdownTimeLabelViewModel
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            if viewModel.time < 0 {
+                Text("+")
+            }
+            TimeLabelView(viewModel: viewModel.timeLabelViewModel)
+        }
+        .font(TiTiFont.HGGGothicssiP60g(size: viewModel.fontSize))
+    }
+}
