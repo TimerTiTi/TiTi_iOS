3c5eff6be8e21b37b2f8dc889f4b8e08b388efdb
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-09 10:50:20 +0900

    [Feat] Setting 창 UI 설정 추가, section count 관련 로직 수정

--- Project_Timer/Setting/Main/SettingVM.swift
+++ Project_Timer/Setting/Main/SettingVM.swift
@@ -21,21 +21,25 @@ final class SettingVM {
     private func configureTitles() {
         self.sectionTitles.append("Introducing app".localized())
         self.sectionTitles.append("Notification".localized())
+        self.sectionTitles.append("UI 설정")
         self.sectionTitles.append("Version & Update history".localized())
         self.sectionTitles.append("Developer".localized())
     }
     
     private func configureCells() {
         // 앱 소개
-        var cells1: [SettingCellInfo] = []
-        cells1.append(SettingCellInfo(title: "TiTi Functions".localized(), nextVCIdentifier: SettingFunctionsListVC.identifier))
-        cells1.append(SettingCellInfo(title: "TiTi Lab".localized(), nextVCIdentifier: SettingTiTiLabVC.identifier))
+        var cells0: [SettingCellInfo] = []
+        cells0.append(SettingCellInfo(title: "TiTi Functions".localized(), nextVCIdentifier: SettingFunctionsListVC.identifier))
+        cells0.append(SettingCellInfo(title: "TiTi Lab".localized(), nextVCIdentifier: SettingTiTiLabVC.identifier))
         // 알림 설정
-        var cells2: [SettingCellInfo] = []
-        cells2.append(SettingCellInfo(title: "Timer".localized(), subTitle: "5 minutes before, and End time".localized(), toggleKey: .timerPushable))
-        cells2.append(SettingCellInfo(title: "Stopwatch".localized(), subTitle: "Every 1 hour passed".localized(), toggleKey: .stopwatchPushable))
+        var cells1: [SettingCellInfo] = []
+        cells1.append(SettingCellInfo(title: "Timer".localized(), subTitle: "5 minutes before, and End time".localized(), toggleKey: .timerPushable))
+        cells1.append(SettingCellInfo(title: "Stopwatch".localized(), subTitle: "Every 1 hour passed".localized(), toggleKey: .stopwatchPushable))
 //        cells2.append(SettingCellInfo(title: "휴식", subTitle: "5분단위 경과시 알림", toggleKey: .restPushable))
-        cells2.append(SettingCellInfo(title: "Update".localized(), subTitle: "Pop-up alert for New version".localized(), toggleKey: .updatePushable))
+        cells1.append(SettingCellInfo(title: "Update".localized(), subTitle: "Pop-up alert for New version".localized(), toggleKey: .updatePushable))
+        // UI 설정
+        var cells2: [SettingCellInfo] = []
+        cells2.append(SettingCellInfo(title: "시간표시", subTitle: "시간변화를 부드럽게 표시합니다", toggleKey: .timelabelsAnimation))
         // 버전 및 업데이트 내역
         var cells3: [SettingCellInfo] = []
         let versionCell = SettingCellInfo(title: "Version Info".localized(), subTitle: "Latest version".localized()+":", rightTitle: String.currentVersion, link: NetworkURL.appstore)
@@ -45,6 +49,6 @@ final class SettingVM {
         // 개발자
         var cells4: [SettingCellInfo] = []
         cells4.append(SettingCellInfo(title: "FDEE"))
-        self.cells = [cells1, cells2, cells3, cells4]
+        self.cells = [cells0, cells1, cells2, cells3, cells4]
     }
 }
