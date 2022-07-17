8ce4869758b65cb70531154243475b317bd30f60
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-17 15:23:33 +0900

    [Feat] tasksCollectionView 표시 로직 구현

---
+++ Project_Timer/Statistics/Cell/StandardDailyTaskCell.swift
@@ -0,0 +1,33 @@
+//
+//  StandardDailyTaskCell.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/07/17.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+final class StandardDailyTaskCell: UICollectionViewCell {
+    static let identifier = "StandardDailyTaskCell"
+    static let height: CGFloat = CGFloat(20)
+    
+    @IBOutlet var checkIcon: UILabel!
+    @IBOutlet var taskNameLabel: UILabel!
+    @IBOutlet var taskTimeLabel: UILabel!
+    @IBOutlet var taskBackgroundView: UIView!
+    
+    func configure(index: Int, taskInfo: TaskInfo) {
+        self.taskNameLabel.text = taskInfo.taskName
+        self.taskTimeLabel.text = taskInfo.taskTime.toTimeString
+        self.updateColor(index: index)
+    }
+    
+    private func updateColor(index: Int) {
+        let startColorIndex = UserDefaultsManager.get(forKey: .startColor) as? Int ?? 1
+        let color = UIColor.graphColor(num: (startColorIndex+index)%12)
+        self.checkIcon.textColor = color
+        self.taskBackgroundView.backgroundColor = color
+        self.taskTimeLabel.textColor = color
+    }
+}
