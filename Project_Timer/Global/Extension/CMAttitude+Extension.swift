3acc82b4c0cb44cb6908b5bb7b56a201eeb86e31
최수정 <babeeboo2000@gmail.com>
2022-07-19 16:21:20 +0900

    [Feat] 모션디텍터 구현

---
+++ Project_Timer/Global/Extension/CMAttitude+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  CMAttitude+Extension.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/07/19.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import CoreMotion
+
+extension CMAttitude {
+    var isFaceDown: Bool {
+        let isRollFaceDown = abs(roll) > 2.2
+        let isPitchFaceDown = abs(pitch) < 0.8
+        
+        return isRollFaceDown && isPitchFaceDown
+    }
+    
+    var isFaceUp: Bool {
+        return !isFaceDown
+    }
+}
---
+++ Project_Timer/Global/MotionDetector.swift
@@ -0,0 +1,54 @@
+//
+//  MotionManager.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/07/18.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+final class MotionDetector {
+    static let shared = MotionDetector()
+    static let orientationDidChangeToFaceDownNotification = Notification.Name("orientationDidChangeToFaceDownNotification")
+    static let orientationDidChangeToFaceUpNotification = Notification.Name("orientationDidChangeToFaceUpNotification")
+    
+    private let motion: CMMotionManager
+    private var isDetecting: Bool
+    private var lastAttitude: CMAttitude?
+    
+    private init() {
+        self.motion = CMMotionManager()
+        self.isDetecting = false
+    }
+    
+    func beginGeneratingMotionNotification() {
+        guard self.motion.isDeviceMotionAvailable else { return }
+        
+        self.isDetecting = true
+        self.motion.deviceMotionUpdateInterval = 0.1
+        self.motion.startDeviceMotionUpdates(to: OperationQueue()) { [weak self] motion, error in
+            guard error == nil,
+                  let self = self,
+                  let newAttitude = motion?.attitude else { return }
+            
+            let oldAttitude = self.lastAttitude ?? newAttitude
+            self.lastAttitude = newAttitude
+            
+            if !oldAttitude.isFaceUp && newAttitude.isFaceUp {
+                NotificationCenter.default.post(name: Self.orientationDidChangeToFaceUpNotification, object: self)
+            }
+            if !oldAttitude.isFaceDown && newAttitude.isFaceDown {
+                NotificationCenter.default.post(name: Self.orientationDidChangeToFaceDownNotification, object: self)
+            }
+        }
+    }
+    
+    func endGeneratingMotionNotification() {
+        guard isDetecting else { return }
+        
+        self.motion.stopDeviceMotionUpdates()
+        self.isDetecting = false
+    }
+}
