587d1565d9e26915a1342bc65ec0cf7bc3513498
최수정 <babeeboo2000@gmail.com>
2022-07-20 00:31:22 +0900

    [Remove] OrientationLocker 제거

--- Project_Timer/AppDelegate.swift
+++ Project_Timer/AppDelegate.swift
@@ -11,8 +11,6 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var orientationLock = UIInterfaceOrientationMask.all
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         /// 앱 실행시 Analytics 에 정보 전달부분
         FirebaseApp.configure()
@@ -95,11 +93,5 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.alert, .badge, .sound])
-    }
-}
-
-extension AppDelegate {
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-            return self.orientationLock
     }
 }
