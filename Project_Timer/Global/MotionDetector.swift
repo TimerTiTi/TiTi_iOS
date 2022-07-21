cf8e06d2dcb8dedd7fcb2ffb69ee6c42dce4f743
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-21 20:30:11 +0900

    [Comment] 주석 추가, motionDetector 주기 및 기준vector값 수정

--- Project_Timer/Global/MotionDetector.swift
+++ Project_Timer/Global/MotionDetector.swift
@@ -17,7 +17,7 @@ final class MotionDetector {
     
     private let motion: CMMotionManager
     private(set) var isDetecting: Bool
-    private var isFaceDown: Bool?
+    private var isFaceDown: Bool? /// 직전값
     
     private init() {
         self.motion = CMMotionManager()
@@ -25,28 +25,29 @@ final class MotionDetector {
     }
     
     func beginGeneratingMotionNotification() {
+        /// 센서가 없는 경우 return
         guard self.motion.isDeviceMotionAvailable else { return }
-        
+        /// 동작과 상관없이 항상 계산이 진행되는 코드
         self.isDetecting = true
-        self.motion.deviceMotionUpdateInterval = 0.05
+        self.motion.deviceMotionUpdateInterval = 0.1 /// 0.1초 간격으로 detect
         self.motion.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: OperationQueue()) { [weak self] motion, error  in
             guard error == nil,
                   let self = self,
                   let gravity = motion?.gravity else { return }
             
-            let isFaceDownNow = gravity.z > 0.85
+            let isFaceDownNow = gravity.z > 0.65 /// 독서대에서 뒤집는 각을 고려한 뒤집었다고 판정되는 vector 기준값
             let isFaceUpNow = !isFaceDownNow
             let isFaceDownBefore = self.isFaceDown ?? isFaceDownNow
             let isFaceUpBefore = !isFaceDownBefore
             
-            if !isFaceUpBefore && isFaceUpNow {
+            if !isFaceUpBefore && isFaceUpNow { /// faceDown -> faceUp 변화시
                 NotificationCenter.default.post(name: Self.orientationDidChangeToFaceUpNotification, object: self)
             }
-            if !isFaceDownBefore && isFaceDownNow {
+            if !isFaceDownBefore && isFaceDownNow { /// faceUp -> faceDown 변화시
                 NotificationCenter.default.post(name: Self.orientationDidChangeToFaceDownNotification, object: self)
             }
             
-            self.isFaceDown = isFaceDownNow
+            self.isFaceDown = isFaceDownNow /// 직건값 update
         }
     }
     
