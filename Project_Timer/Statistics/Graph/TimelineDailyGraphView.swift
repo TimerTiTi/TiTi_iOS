e35159132a818dde9b016d51d07c8a93b8b39600
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-23 14:17:55 +0900

    [Feat] TimelineDailyGraphView dateLabel 추가

--- Project_Timer/Statistics/Graph/TimelineDailyGraphView.swift
+++ Project_Timer/Statistics/Graph/TimelineDailyGraphView.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 final class TimelineDailyGraphView: UIView {
+    private var dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = TiTiFont.HGGGothicssiP80g(size: 25)
+        label.textColor = UIColor.label
+        NSLayoutConstraint.activate([
+            label.heightAnchor.constraint(equalToConstant: 27.5)
+        ])
+        label.textAlignment = .center
+        label.text = "0000.00.00"
+        return label
+    }()
     private var contentView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -40,10 +52,38 @@ final class TimelineDailyGraphView: UIView {
             self.contentView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
         
+        self.contentView.addSubview(self.dateLabel)
+        NSLayoutConstraint.activate([
+            self.dateLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 15),
+            self.dateLabel.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor)
+        ])
+        
         self.contentView.configureShadow()
     }
-    
+}
+
+// MARK: TimelineDailyGraphView Public Actions
+extension TimelineDailyGraphView {
+    /// dark, light mode 변경의 경우
     func updateDarkLightMode() {
         self.contentView.configureShadow()
+    }
+    /// daily 변경, 또는 color 변경이 경우
+    func updateFromDaily(_ daily: Daily?) {
+        self.updateDateLabel(daily?.day)
+    }
+}
+
+// MARK: TimelineDailyGraphView Private Actions
+extension TimelineDailyGraphView {
+    private func updateDateLabel(_ day: Date?) {
+        guard let day = day else {
+            self.dateLabel.text = "0000.00.00"
+            return
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY.MM.dd"
+        self.dateLabel.text = dateFormatter.string(from: day)
     }
 }
