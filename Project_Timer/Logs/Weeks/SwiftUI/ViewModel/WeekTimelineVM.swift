793c474bb6452cecbc45cdb071805aa1822ee170
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-30 17:35:14 +0900

    [Feat] WeekTimelineVM 생성 및 VM 로직 구현 완료

--- Project_Timer/Logs/Weeks/SwiftUI/ViewModel/WeekTimelineVM.swift
+++ Project_Timer/Logs/Weeks/SwiftUI/ViewModel/WeekTimelineVM.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+final class WeekTimelineVM: ObservableObject {
+    @Published var color1Index: Int = 2
+    @Published var color2Index: Int = 1
+    @Published var weekTimes: [WeekTimeBlock] = []
+    
+    init() {
+        self.updateColor(isReversColor: false)
+        self.resetTimes()
+    }
+    
+    func update(weekData: DailysWeekData) {
+        var weekTimes = self.weekTimes
+        for idx in 0...6 {
+            let day = weekData.weekDates[idx].MDstyleString
+            if let daily = weekData.dailys[idx] {
+                weekTimes[idx] = WeekTimeBlock(id: idx, day: day, sumTime: daily.totalTime)
+            } else {
+                weekTimes[idx] = WeekTimeBlock(id: idx, day: day, sumTime: 0)
+            }
+        }
+        self.weekTimes = weekTimes
+    }
+    
+    func updateColor(isReversColor: Bool) {
+        let userColorIndex = UserDefaultsManager.get(forKey: .startColor) as? Int ?? 1
+        if isReversColor {
+            self.color1Index = (userColorIndex-1+12)%12 == 0 ? 12 : (userColorIndex-1+12)%12
+            self.color2Index = userColorIndex
+        } else {
+            self.color1Index = (userColorIndex+1+12)%12 == 0 ? 12 : (userColorIndex+1+12)%12
+            self.color2Index = userColorIndex
+        }
+    }
+    
+    private func resetTimes() {
+        self.weekTimes = (0...6).map { WeekTimeBlock(id: $0, day: "-/-", sumTime: 0) }
+    }
+}
