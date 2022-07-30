91f11e5bee2119da891646b23ae4b3e897760e49
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-30 21:15:35 +0900

    [Refactor] collectionView 크기 미세조정

--- Project_Timer/Logs/Weeks/Cell/StandardWeekTaskCell.swift
+++ Project_Timer/Logs/Weeks/Cell/StandardWeekTaskCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class StandardWeekTaskCell: UICollectionViewCell {
     static let identifier = "StandardWeekTaskCell"
-    static let size = CGSize(width: CGFloat(215), height: CGFloat(27))
+    static let size = CGSize(width: CGFloat(223), height: CGFloat(27.22))
     
     @IBOutlet var topLabel: UILabel!
     @IBOutlet var taskNameLabel: UILabel!
