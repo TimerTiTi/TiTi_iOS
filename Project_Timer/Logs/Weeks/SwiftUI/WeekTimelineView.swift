840048af6795ac9ac415aa2198656edf16bfc6f8
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-30 13:53:37 +0900

    [Feat] Weeks 폴더링 내 구조 생성

---
+++ Project_Timer/Logs/Weeks/SwiftUI/WeekTimelineView.swift
@@ -0,0 +1,21 @@
+//
+//  WeekTimelineView.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/07/30.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct WeekTimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeekTimelineView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeekTimelineView()
+    }
+}
---
+++ Project_Timer/Logs/Weeks/WeeksVM.swift
@@ -0,0 +1,13 @@
+//
+//  WeeksVM.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/07/30.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import Foundation
+
+class WeeksVM {
+    
+}
