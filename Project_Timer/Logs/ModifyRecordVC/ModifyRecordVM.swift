6ee68a5c068b8562c502175c0ac2f173051ceb30
최수정 <babeeboo2000@gmail.com>
2022-08-14 19:37:52 +0900

    [Feat] 그래프 데이터 연결

--- Project_Timer/Logs/ModifyRecordVC/ModifyRecordVM.swift
+++ Project_Timer/Logs/ModifyRecordVC/ModifyRecordVM.swift
@@ -17,4 +17,15 @@ final class ModifyRecordVM {
     init() {
         self.timelineVM = TimelineVM()
     }
+    
+    func updateDaily(to daily: Daily?) {
+        self.currentDaily = daily
+        self.timelineVM.update(daily: daily)
+        guard let tasks = daily?.tasks else {
+            self.tasks = []
+            return
+        }
+        self.tasks = tasks.sorted(by: { $0.value > $1.value })
+            .map { TaskInfo(taskName: $0.key, taskTime: $0.value) }
+    }
 }
