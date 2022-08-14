6322120305795e880cf926cd4eb20240c93e7655
최수정 <babeeboo2000@gmail.com>
2022-08-14 19:16:27 +0900

    [Feat] 그래프 스크롤뷰 및 페이지컨트롤 추가

--- Project_Timer/Logs/ModifyRecordVC/ModifyRecordVC.swift
+++ Project_Timer/Logs/ModifyRecordVC/ModifyRecordVC.swift
@@ -11,14 +11,61 @@ import UIKit
 final class ModifyRecordVC: UIViewController {
     static let identifier = "ModifyRecordVC"
     
+    @IBOutlet weak var graphsScrollView: UIScrollView!
+    @IBOutlet weak var graphsContentView: UIView!
+    @IBOutlet weak var graphsPageControl: UIPageControl!
+    private var standardDailyGraphView = StandardDailyGraphView()
+    private var timelineDailyGraphView = TimelineDailyGraphView()
+    private var tasksProgressDailyGraphView = TasksProgressDailyGraphView()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "ModifyRecordVC"
+        self.configureScrollView()
+        self.configureGraphs()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
+}
 
+extension ModifyRecordVC {
+    private func configureScrollView() {
+        self.graphsScrollView.delegate = self
+    }
+    
+    private func configureGraphs() {
+        self.graphsContentView.addSubview(self.standardDailyGraphView)
+        NSLayoutConstraint.activate([
+            self.standardDailyGraphView.topAnchor.constraint(equalTo: self.graphsContentView.topAnchor),
+            self.standardDailyGraphView.leadingAnchor.constraint(equalTo: self.graphsContentView.leadingAnchor),
+            self.standardDailyGraphView.bottomAnchor.constraint(equalTo: self.graphsContentView.bottomAnchor)
+        ])
+        
+        self.graphsContentView.addSubview(self.timelineDailyGraphView)
+        NSLayoutConstraint.activate([
+            self.timelineDailyGraphView.topAnchor.constraint(equalTo: self.graphsContentView.topAnchor),
+            self.timelineDailyGraphView.leadingAnchor.constraint(equalTo: self.standardDailyGraphView.trailingAnchor),
+            self.timelineDailyGraphView.bottomAnchor.constraint(equalTo: self.graphsContentView.bottomAnchor)
+        ])
+        
+        self.graphsContentView.addSubview(self.tasksProgressDailyGraphView)
+        NSLayoutConstraint.activate([
+            self.tasksProgressDailyGraphView.topAnchor.constraint(equalTo: self.graphsContentView.topAnchor),
+            self.tasksProgressDailyGraphView.leadingAnchor.constraint(equalTo: self.timelineDailyGraphView.trailingAnchor),
+            self.tasksProgressDailyGraphView.bottomAnchor.constraint(equalTo: self.graphsContentView.bottomAnchor),
+            self.tasksProgressDailyGraphView.trailingAnchor.constraint(equalTo: self.graphsContentView.trailingAnchor)
+        ])
+    }
+}
+
+extension ModifyRecordVC: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard scrollView == self.graphsScrollView else { return }
+        let value = scrollView.contentOffset.x/scrollView.frame.size.width
+        self.graphsPageControl.currentPage = Int(round(value))
+    }
 }
