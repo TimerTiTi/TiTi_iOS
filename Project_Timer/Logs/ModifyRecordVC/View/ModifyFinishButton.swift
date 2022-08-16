90bff8610c30b8cd9d715ec7aa437e74d88678d3
최수정 <babeeboo2000@gmail.com>
2022-08-16 12:04:40 +0900

    [Feat] 과목추가 인터렉션뷰 구현

---
+++ Project_Timer/Logs/ModifyRecordVC/View/ModifyFinishButton.swift
@@ -0,0 +1,35 @@
+//
+//  ModifyFinishButton.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/08/16.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+class ModifyFinishButton: UIButton {
+    override var isEnabled: Bool {
+        didSet {
+            if isEnabled {
+                self.backgroundColor = UIColor(named: String.userTintColor)
+            } else {
+                self.backgroundColor = UIColor.systemGray5
+            }
+        }
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect())
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: 30),
+            self.widthAnchor.constraint(equalToConstant: 70)
+        ])
+        self.cornerRadius = 6
+        self.setTitleColor(UIColor.label, for: .normal)
+        self.setTitleColor(UIColor.systemGray2, for: .disabled)
+        self.titleLabel?.font = TiTiFont.HGGGothicssiP60g(size: 18)
+    }
+}
