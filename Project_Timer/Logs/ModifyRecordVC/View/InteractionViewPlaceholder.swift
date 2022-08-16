952de9d1acc44c61ad452c521860a1d4331a0ac3
최수정 <babeeboo2000@gmail.com>
2022-08-16 16:01:16 +0900

    [Refactor] emptyView -> placeholder로 변경

---
+++ Project_Timer/Logs/ModifyRecordVC/View/InteractionViewPlaceholder.swift
@@ -0,0 +1,34 @@
+//
+//  InteractionViewPlaceholder.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/08/16.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+class InteractionViewPlaceholder: UIView {
+    private var messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "과목을 선택하여 기록수정 후\nSAVE를 눌러주세요"
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.font = TiTiFont.HGGGothicssiP60g(size: 17)
+        label.textColor = UIColor.label
+        return label
+    }()
+    
+    convenience init() {
+        self.init(frame: CGRect())
+        
+        self.addSubview(messageLabel)
+        NSLayoutConstraint.activate([
+            self.messageLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            self.messageLabel.rightAnchor.constraint(equalTo: self.rightAnchor),
+            self.messageLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.messageLabel.leftAnchor.constraint(equalTo: self.leftAnchor)
+        ])
+    }
+}
