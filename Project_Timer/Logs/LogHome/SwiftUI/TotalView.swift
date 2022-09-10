c9bff62e14ab56f892abc476c8488543c12178d9
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-09-10 21:21:03 +0900

    [Refactor] TotalView font size 변경, monthLayer 추가

--- Project_Timer/Logs/LogHome/SwiftUI/TotalView.swift
+++ Project_Timer/Logs/LogHome/SwiftUI/TotalView.swift
@@ -75,22 +75,23 @@ struct TotalTaskCellView : View {
         VStack(alignment: .center, spacing: 4) {
             HStack(alignment: .center, spacing: 0) {
                 Text("Top\(rank)")
-                    .font(TiTiFont.HGGGothicssiP80g(size: 10))
+                    .font(TiTiFont.HGGGothicssiP80g(size: 10.5))
                     .foregroundColor(TiTiColor.graphColor(num: colorIndex).toColor)
                     .frame(width: 24, alignment: .leading)
                 Text("\(taskName)")
-                    .font(TiTiFont.HGGGothicssiP60g(size: 10.5))
+                    .font(TiTiFont.HGGGothicssiP60g(size: 11))
                     .padding(.horizontal, 2)
                     .background(TiTiColor.graphColor(num: colorIndex).withAlphaComponent(0.5).toColor)
                     .frame(width: 134, alignment: .leading)
                     .lineLimit(1)
                 Spacer(minLength: 2)
                 Text("\(taskTime/3600) H")
-                    .font(TiTiFont.HGGGothicssiP80g(size: 10))
+                    .font(TiTiFont.HGGGothicssiP80g(size: 10.5))
                     .foregroundColor(TiTiColor.graphColor(num: colorIndex).toColor)
             }
             Divider()
-                .background(UIColor.lightGray.withAlphaComponent(0.5).toColor)
+                .frame(height: 1)
+                .background(UIColor(named: "System_border")?.toColor)
         }
         .frame(width: 188, height: 25, alignment: .trailing)
     }
