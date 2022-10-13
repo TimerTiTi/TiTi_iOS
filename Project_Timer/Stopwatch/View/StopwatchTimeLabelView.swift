2a1522cd3555b3a189ef6348ff2a791663e5e860
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-10-13 18:56:32 +0900

    [Refactor] StopwatchTimeLabelView 및 VM 수정 반영

---
+++ Project_Timer/Stopwatch/View/StopwatchTimeLabelView.swift
@@ -0,0 +1,44 @@
+//
+//  StopwatchTimeLabelView.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/06/27.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct StopwatchTimeLabelView: View {
+    @ObservedObject var viewModel: StopwatchTimeLabelVM
+    @ObservedObject var baseViewModel: BaseTimeLabelVM
+    
+    init(viewModel: StopwatchTimeLabelVM) {
+        self.viewModel = viewModel
+        self.baseViewModel = viewModel.timeLabelVM
+    }
+    
+    var color: Color {
+        if baseViewModel.isRunning {
+            return Color.stopWatchColor
+        } else {
+            return baseViewModel.isWhite ? .white : .black.opacity(0.5)
+        }
+    }
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            if baseViewModel.timeLabel.hourTens > 0 {
+                BaseSingleTimeLabelView(viewModel: baseViewModel.hourTensViewModel)
+            }
+            BaseSingleTimeLabelView(viewModel: baseViewModel.hourUnitsViewModel)
+            Text(":")
+            BaseSingleTimeLabelView(viewModel: baseViewModel.minuteTensViewModel)
+            BaseSingleTimeLabelView(viewModel: baseViewModel.minuteUnitsViewModel)
+            Text(":")
+            BaseSingleTimeLabelView(viewModel: baseViewModel.secondTensViewModel)
+            BaseSingleTimeLabelView(viewModel: baseViewModel.secondUnitsViewModel)
+        }
+        .font(TiTiFont.HGGGothicssiP60g(size: baseViewModel.fontSize))
+        .foregroundColor(self.color)
+    }
+}
---
+++ Project_Timer/Stopwatch/ViewModel/StopwatchTimeLabelVM.swift
@@ -0,0 +1,29 @@
+//
+//  StopwatchTimeLabelVM.swift
+//  Project_Timer
+//
+//  Created by 최수정 on 2022/06/27.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+class StopwatchTimeLabelVM: ObservableObject {
+    @Published var timeLabelVM: BaseTimeLabelVM
+    
+    init(time: Int, fontSize: CGFloat, isWhite: Bool) {
+        self.timeLabelVM = BaseTimeLabelVM(time: time, fontSize: fontSize, isWhite: isWhite)
+    }
+    
+    func updateTime(_ newTime: Int, showsAnimation: Bool) {
+        self.timeLabelVM.updateTime(newTime, showsAnimation: showsAnimation)
+    }
+    
+    func updateRunning(to isRunning: Bool) {
+        self.timeLabelVM.isRunning = isRunning
+    }
+    
+    func updateIsWhite(to isWhite: Bool) {
+        self.timeLabelVM.isWhite = isWhite
+    }
+}
