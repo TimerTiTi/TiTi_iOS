5dbf458b89e738f1664d714c98f5c4de6d8b550b
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-10-14 00:51:35 +0900

    [Refactor] colorSelector 레인보우띠 형식의 버튼으로 수정, ver7.7 update

--- Project_Timer/Timer/TimerViewController.swift
+++ Project_Timer/Timer/TimerViewController.swift
@@ -32,6 +32,7 @@ class TimerViewController: UIViewController {
     @IBOutlet var setTimerBT: UIButton!
     @IBOutlet var settingBT: UIButton!
     @IBOutlet weak var colorSelector: UIButton!
+    @IBOutlet weak var colorSelectorBorderView: UIImageView!
     @IBOutlet weak var todayLabel: UILabel!
     @IBOutlet weak var warningRecordDate: UIButton!
 
@@ -355,6 +356,7 @@ extension TimerViewController {
         self.timerLabel.textColor = .white
         self.targetTimeLabel.textColor = .white
         self.finishTimeLabel.textColor = .white
+        self.colorSelector.backgroundColor = .black
         //예상종료시간 숨기기, stop 버튼 센터로 이동
         UIView.animate(withDuration: 0.3, animations: {
             self.settingBT.alpha = 0
@@ -363,7 +365,7 @@ extension TimerViewController {
             self.startStopBTLabel.textColor = self.RED!
             self.startStopBT.layer.borderColor = UIColor.clear.cgColor
             self.startStopBTLabel.text = "◼︎"
-            self.colorSelector.alpha = 0
+            self.colorSelectorBorderView.alpha = 0
             self.tabBarController?.tabBar.isHidden = true
             self.todayLabel.alpha = 0
         })
@@ -387,6 +389,7 @@ extension TimerViewController {
         self.finishTimeLabel.textColor = self.textColor
         self.settingBT.tintColor = self.textColor
         self.setTimerBT.tintColor = self.textColor
+        self.colorSelector.backgroundColor = self.backgroundColor
         //예상종료시간 보이기, stop 버튼 제자리로 이동
         UIView.animate(withDuration: 0.3, animations: {
             self.settingBT.alpha = 1
@@ -395,8 +398,7 @@ extension TimerViewController {
             self.startStopBTLabel.textColor = self.textColor
             self.startStopBT.layer.borderColor = self.startButtonColor?.cgColor
             self.startStopBTLabel.text = "▶︎"
-            self.colorSelector.alpha = 0.7
-            self.colorSelector.layer.borderColor = self.textColor.cgColor
+            self.colorSelectorBorderView.alpha = 1
             self.tabBarController?.tabBar.isHidden = false
         })
         //animation test
