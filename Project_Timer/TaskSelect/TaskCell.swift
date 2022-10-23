c4efca8e092f77e18f9acfa7ab230470d66f6d0d
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-10-23 19:13:42 +0900

    [Refactor] TaskCell 표시 구현, toggle에 따른 animation 구현

--- Project_Timer/TaskSelect/TaskCell.swift
+++ Project_Timer/TaskSelect/TaskCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class TaskCell: UITableViewCell {
     static let identifier = "TaskCell"
-    static let height: CGFloat = 52
+    static let height: CGFloat = 54
     
     @IBOutlet weak var taskNameLabel: UILabel!
     @IBOutlet weak var taskTargetTimesStackView: UIStackView!
@@ -19,6 +19,7 @@ final class TaskCell: UITableViewCell {
     @IBOutlet weak var editTaskTargetTimeButton: UIButton!
     @IBOutlet weak var taskTargetTimeSwitch: UISwitch!
     
+    @IBOutlet weak var taskNameLabelTopConst: NSLayoutConstraint!
     var toggleTargetTime: ((Bool) -> Void)?
     var editTargetTime: (() -> Void)?
     
@@ -29,6 +30,12 @@ final class TaskCell: UITableViewCell {
     }
     
     @IBAction func toggleTargetTime(_ sender: UISwitch) {
+        UIView.animate(withDuration: 0.3) {
+            self.taskNameLabelTopConst.constant = sender.isOn ? 8 : 15
+            self.taskTargetTimesStackView.alpha = sender.isOn ? 1 : 0
+            self.editTaskTargetTimeButton.alpha = sender.isOn ? 1 : 0
+            self.layoutIfNeeded()
+        }
         self.toggleTargetTime?(sender.isOn)
     }
     
@@ -40,9 +47,11 @@ final class TaskCell: UITableViewCell {
         self.configureColor(color: color)
         self.taskNameLabel.text = task.taskName
         self.taskTargetTimeSwitch.isOn = task.isTaskTargetTimeOn
-        self.taskTargetTimeLabel.text = "Target Time:".localized()
+        self.taskTargetTimeLabel.text = "Setted Target Time:".localized()
         self.taskTargetTime.text = task.taskTargetTime.toTimeString
+        self.taskNameLabelTopConst.constant = task.isTaskTargetTimeOn ? 8 : 15
         self.taskTargetTimesStackView.alpha = task.isTaskTargetTimeOn ? 1 : 0
+        self.editTaskTargetTimeButton.alpha = task.isTaskTargetTimeOn ? 1 : 0
     }
 }
 
