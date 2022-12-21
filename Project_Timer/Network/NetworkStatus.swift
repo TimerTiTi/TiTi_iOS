64e5601b20107ababee2e213593546498fcadfc5
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-12-21 12:38:14 +0900

    [Feat] NetworkStatus case 추가, TestServerAuth protocol 추가

--- Project_Timer/Network/NetworkStatus.swift
+++ Project_Timer/Network/NetworkStatus.swift
@@ -13,8 +13,24 @@ struct NetworkResult {
     let statusCode: Int?
 }
 
-enum NetworkStatus {
+enum NetworkStatus: Int {
     case SUCCESS // 200
-    case DECODEERROR
-    case FAIL
+    case FAIL // 400
+    case AUTHENTICATION // 401
+    case NOTFOUND // 404
+    case CONFLICT // 409
+    case SERVERERROR // 500
+    case DECODEERROR // -1
+    
+    func status(statusCode: Int) -> NetworkStatus {
+        switch statusCode {
+        case 200: return .SUCCESS
+        case 401: return .AUTHENTICATION
+        case 404: return .NOTFOUND
+        case 409: return .CONFLICT
+        case 500: return .SERVERERROR
+        case -1: return .DECODEERROR
+        default: return .FAIL
+        }
+    }
 }
