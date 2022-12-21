53b1710b288ac5a91977c038c2ad665096fede61
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-12-21 12:38:14 +0900

    [Feat] NetworkStatus case 추가, TestServerAuth protocol 추가

---
+++ Project_Timer/Network/DTO/TestServer/TestUserLoginInfo.swift
@@ -0,0 +1,14 @@
+//
+//  TestUserLoginInfo.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/12/21.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import Foundation
+
+struct TestUserLoginInfo {
+    let username: String
+    let password: String
+}
