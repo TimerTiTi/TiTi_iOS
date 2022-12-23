1b6b53ae23445e9a5af5524122f59d7825b5c2ae
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-12-23 16:52:55 +0900

    [Refactor] SyncUserStatusView customView 로 구조 수정 및 localized 반영

---
+++ Project_Timer/Setting/TestServer/SyncDailys/SyncUserStatusView.swift
@@ -0,0 +1,33 @@
+//
+//  SyncUserStatusView.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2022/12/23.
+//  Copyright © 2022 FDEE. All rights reserved.
+//
+
+import UIKit
+
+final class SyncUserStatusView: UIView {
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var deviceDateLabel: UILabel!
+    @IBOutlet weak var serverDateLabel: UILabel!
+    @IBOutlet weak var useage1Label: UILabel!
+    @IBOutlet weak var useage2Label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.configureLocalized()
+        self.configureUsername()
+    }
+    
+    private func configureLocalized() {
+        self.useage1Label.text = "The last synchronization time of the current device and the latest synchronization time reflected in the server.".localized()
+        self.useage2Label.text = "Synchronization backs up your modified data and allows you to receive data backed up from other devices.".localized()
+    }
+    
+    private func configureUsername() {
+        guard let username = KeyChain.shared.get(key: .username) else { return }
+        self.usernameLabel.text = username
+    }
+}
