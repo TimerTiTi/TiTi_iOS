84409a62de518d58efd9fe3badc481a274fbedfc
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-01-03 23:57:00 +0900

    [Refactor] NetworkStatus: 304 추가

--- Project_Timer/Network/NetworkStatus.swift
+++ Project_Timer/Network/NetworkStatus.swift
@@ -15,6 +15,7 @@ struct NetworkResult {
 
 enum NetworkStatus: String {
     case SUCCESS // 200~204
+    case NOTMODIFIED // 304
     case FAIL // 400
     case AUTHENTICATION // 401
     case NOTFOUND // 404
@@ -27,6 +28,7 @@ enum NetworkStatus: String {
         case 200: return .SUCCESS
         case 201: return .SUCCESS
         case 204: return .SUCCESS
+        case 304: return .SUCCESS
         case 401: return .AUTHENTICATION
         case 404: return .NOTFOUND
         case 409: return .CONFLICT
