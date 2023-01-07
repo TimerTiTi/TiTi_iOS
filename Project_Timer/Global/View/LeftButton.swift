528410edbb31b6854ab49ded909021acc8498d69
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-01-07 15:37:58 +0900

    [Feat] WeekVC horizontal buttons 추가 반영

--- Project_Timer/Global/View/LeftButton.swift
+++ Project_Timer/Global/View/LeftButton.swift
@@ -14,9 +14,15 @@ final class LeftButton: UIButton {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.setImage(UIImage(systemName: "chevron.left"), for: .normal)
         self.tintColor = UIColor.label
+        let padding: CGFloat = 12
+        if #available(macCatalyst 15.0, *) {
+            self.configuration?.contentInsets = .init(top: padding, leading: padding, bottom: padding, trailing: padding)
+        } else {
+            self.contentEdgeInsets = .init(top: padding, left: padding, bottom: padding, right: padding)
+        }
         NSLayoutConstraint.activate([
-            self.widthAnchor.constraint(equalToConstant: 26),
-            self.heightAnchor.constraint(equalToConstant: 26)
+            self.widthAnchor.constraint(equalToConstant: 50),
+            self.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
