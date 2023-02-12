091ab9e7f35a1ac0b0b4bb91c34edb9c991df2da
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-02-12 18:01:12 +0900

    [Feat] LockScreen LiveActivity 첫동작 구현

---
+++ widget/TiTiLockscreenAttributes.swift
@@ -0,0 +1,22 @@
+//
+//  TiTiLockscreenAttributes.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/02/12.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import Foundation
+import ActivityKit
+
+struct TiTiLockscreenAttributes: ActivityAttributes {
+    public typealias titiStatus = ContentState
+    
+    public struct ContentState: Codable, Hashable {
+        var taskName: String
+        var timer: ClosedRange<Date>
+    }
+    
+    var isTimer: Bool
+    var colorIndex: Int
+}
