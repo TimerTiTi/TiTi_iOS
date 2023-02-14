79e376135b97c15f962443ab557a75cdaa3eb055
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-02-14 18:02:19 +0900

    [Fix] 컬러 날아가는 버그 수정, liveActivity light모드 배경색 변경

--- widget/widgetLiveActivity.swift
+++ widget/widgetLiveActivity.swift
@@ -51,7 +51,7 @@ struct widgetLiveActivity: Widget {
     }
     
     func color(context: ActivityViewContext<TiTiLockscreenAttributes>) -> Color {
-        if let color = UserDefaults.shared.colorForKey(key: context.attributes.isTimer ? .timerBackground : .stopwatchBackground) {
+        if let color = UserDefaults.colorForKey(key: context.attributes.isTimer ? .timerBackground : .stopwatchBackground) {
             return Color(color)
         } else {
             return Color(UIColor(named: context.attributes.isTimer ? "Background" : "Background2")!)
@@ -68,7 +68,7 @@ struct LockScreenLiveActivityView: View {
             VStack(spacing: -3) {
                 Spacer(minLength: 14)
                 Text("\(context.state.taskName)")
-                    .foregroundColor(.primary.opacity(0.5))
+                    .foregroundColor(.white.opacity(0.5))
                 Text(timerInterval: context.state.timer, countsDown: context.attributes.isTimer)
                     .multilineTextAlignment(.center)
                     .monospacedDigit()
@@ -76,11 +76,12 @@ struct LockScreenLiveActivityView: View {
                     .foregroundColor(color(context: context).opacity(0.5))
                 Spacer()
             }
-            .background(Color(UIColor.systemBackground).opacity(0.6))
+            .background(.black.opacity(0.6))
         } else {
             VStack(spacing: -3) {
                 Spacer(minLength: 14)
                 Text("\(context.state.taskName)")
+                    .foregroundColor(.white)
                 Text(timerInterval: context.state.timer, countsDown: context.attributes.isTimer)
                     .multilineTextAlignment(.center)
                     .monospacedDigit()
@@ -88,12 +89,12 @@ struct LockScreenLiveActivityView: View {
                     .foregroundColor(color(context: context))
                 Spacer()
             }
-            .background(Color(UIColor.systemBackground).opacity(0.6))
+            .background(.black.opacity(0.6))
         }
     }
     
     func color(context: ActivityViewContext<TiTiLockscreenAttributes>) -> Color {
-        if let color = UserDefaults.shared.colorForKey(key: context.attributes.isTimer ? .timerBackground : .stopwatchBackground) {
+        if let color = UserDefaults.colorForKey(key: context.attributes.isTimer ? .timerBackground : .stopwatchBackground) {
             return Color(color)
         } else {
             return Color(UIColor(named: context.attributes.isTimer ? "Background" : "Background2")!)
