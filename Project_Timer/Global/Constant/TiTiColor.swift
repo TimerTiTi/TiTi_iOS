acabdaa5c439c6d6656f3d1c8653c14927c5facf
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-03-21 23:33:24 +0900

    #70 refac: TiTiColor 미사용변수 제거, Assets 미사용컬러 제거

--- Project_Timer/Global/Constant/TiTiColor.swift
+++ Project_Timer/Global/Constant/TiTiColor.swift
@@ -13,13 +13,9 @@ enum TiTiColor {
     static let timerBackground = UIColor(named: "timerBackground")
     static let stopwatchBackground = UIColor(named: "stopwatchBackground")
     static let warningRed = UIColor(named: "warningRed")
-    static let darkRed = UIColor(named: "darkRed")
-    static let dock = UIColor(named: "dock")
     static let startButton = UIColor(named: "startButtonColor")
     static let tabbarNonSelect = UIColor(named: "tabbarNonSelectColor")
-    static let lightpink = UIColor(named: "lightpink")
-    static let D1 = UIColor(named: "D1")
-    static let D2 = UIColor(named: "D2")
+    static let noTaskWarningRed = UIColor(named: "noTaskWarningRed")
     static let tabbarBackground = UIColor(named: "tabbarBackground")
     static func graphColor(num: Int) -> UIColor {
         return UIColor(named: "D\(num)") ?? .blue
