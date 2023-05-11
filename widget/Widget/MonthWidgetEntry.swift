ba812b0758b742f5de62d9f24dfae995c7c98494
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-05-11 17:52:56 +0900

    #72 feat: MonthWidgetEntry 생성 및 MonthWidgetEntryView 수정

---
+++ widget/Widget/MonthWidgetEntry.swift
@@ -0,0 +1,15 @@
+//
+//  MonthWidgetEntry.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/05/11.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import Foundation
+import WidgetKit
+
+struct MonthWidgetEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationIntent
+}
