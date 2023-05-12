8f9f4a33acb5f9219594607fc2fcde2da637cd8e
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-05-12 16:57:34 +0900

    #72 feat: MonthWidgetData 생성, Intent 설정값을 통한 MonthWidgetView 에서 컬러 표시 구현

--- Widget/Widgets/MonthWidget/MonthWidget.swift
+++ Widget/Widgets/MonthWidget/MonthWidget.swift
@@ -26,33 +26,33 @@ struct MonthWidget: Widget {
 
 // MARK: Entry를 받아 상황에 맞는 SwiftUI 뷰를 반환하는 부분
 struct MonthWidgetEntryView: View {
+    var entry: MonthWidgetEntry
     @Environment(\.widgetFamily) var family
-    var entry: MonthWidgetProvider.Entry
     
     @ViewBuilder
     var body: some View {
         // MARK: 만약 동일한 위젯을 사이즈별로 제공하고 싶은 경우: switch family 로 분기처리 하면 된다.
-        MonthWidgetView()
+        MonthWidgetView(entry.data)
     }
 }
 
 struct MonthWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            MonthWidgetEntryView(entry: MonthWidgetEntry(date: Date(), configuration: SelectColorIntent()))
+            MonthWidgetEntryView(entry: MonthWidgetEntry(data: MonthWidgetData(color: .D1)))
                 .previewDevice("iPad (9th generation)")
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
             
-            MonthWidgetEntryView(entry: MonthWidgetEntry(date: Date(), configuration: SelectColorIntent()))
+            MonthWidgetEntryView(entry: MonthWidgetEntry(data: MonthWidgetData(color: .D1)))
                 .previewDevice("iPhone 13 Pro")
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
             
-            MonthWidgetEntryView(entry: MonthWidgetEntry(date: Date(), configuration: SelectColorIntent()))
+            MonthWidgetEntryView(entry: MonthWidgetEntry(data: MonthWidgetData(color: .D1)))
                 .redacted(reason: .placeholder)
                 .previewDevice("iPhone 13 Pro")
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
             
-            MonthWidgetEntryView(entry: MonthWidgetEntry(date: Date(), configuration: SelectColorIntent()))
+            MonthWidgetEntryView(entry: MonthWidgetEntry(data: MonthWidgetData(color: .D1)))
                 .previewDevice("iPad Pro (12.9-inch) (6th generation)")
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
         }
