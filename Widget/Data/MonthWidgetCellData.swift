46269f30be8767565937b1b55df84ef2dfcc6fa1
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-05-13 17:36:39 +0900

    #72 feat: MonthWidgetData 생성, 랜덤한 값으로 날짜별 시간 표시

---
+++ Widget/Data/MonthWidgetCellData.swift
@@ -0,0 +1,16 @@
+//
+//  MonthWidgetCellData.swift
+//  widgetExtension
+//
+//  Created by Kang Minsang on 2023/05/13.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import Foundation
+
+struct MonthWidgetCellData {
+    let day: Int
+    let now: Int
+    let time: Int
+    let color: String
+}
