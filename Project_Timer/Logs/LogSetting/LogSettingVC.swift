ae3cca20d3b230e838fc7602fada26ec3b69029b
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-05-20 23:17:56 +0900

    #80 feat: LogSettingVC 추가

---
+++ Project_Timer/Logs/LogSetting/LogSettingVC.swift
@@ -0,0 +1,18 @@
+//
+//  LogSettingVC.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/05/20.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import UIKit
+
+final class LogSettingVC: UIViewController {
+    static let identifier = "LogSettingVC"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .secondarySystemGroupedBackground
+    }
+}
