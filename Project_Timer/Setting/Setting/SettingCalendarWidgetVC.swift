4100fb9247a85d350c7239d685254987be7e0a1e
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-05-24 11:55:02 +0900

    #82 feat: descriptionLabel 추가 및 표시

--- Project_Timer/Setting/Setting/SettingCalendarWidgetVC.swift
+++ Project_Timer/Setting/Setting/SettingCalendarWidgetVC.swift
@@ -9,9 +9,32 @@
 import UIKit
 
 final class SettingCalendarWidgetVC: UIViewController {
+    private var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        label.text = "Widget shows the top 5 tasks and the recording time by date.".localized()
+        label.textColor = .lightGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Widget".localized()
+        self.configureUI()
+    }
+}
+
+extension SettingCalendarWidgetVC {
+    private func configureUI() {
+        self.title = "Calendar widget".localized()
         self.view.backgroundColor = .systemGroupedBackground
+        
+        self.view.addSubview(self.descriptionLabel)
+        NSLayoutConstraint.activate([
+            self.descriptionLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            self.descriptionLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            self.descriptionLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
     }
 }
