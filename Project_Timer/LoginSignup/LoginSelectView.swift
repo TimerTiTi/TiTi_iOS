11ab31531a24913a3893d0285d72d15ed8e46744
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-09-24 13:47:52 +0900

    feat #91: Asset에 loginLogo, loginBackground, googleLogo 추가

---
+++ Project_Timer/LoginSignup/LoginSelectView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginSelectView.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/09/24.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginSelectView()
+}
