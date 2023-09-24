e9c726845c03a0633593204abac0a9bf8e8338e1
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-09-24 13:51:13 +0900

    feat #91: AppleLoginButton 생성

---
+++ Project_Timer/LoginSignup/AppleLoginButton.swift
@@ -0,0 +1,41 @@
+//
+//  AppleLoginButton.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/09/24.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppleLoginButton: View {
+    var action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            ZStack{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                    .shadow(color: .gray.opacity(0.1), radius: 4, x: 1, y: 2)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 58)
+                
+                HStack(spacing: 12) {
+                    Text("")
+                        .font(.system(size: 30))
+                    Text("Apple로 로그인")
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                }
+            }
+        }
+        .foregroundColor(.black)
+    }
+}
+
+#Preview {
+    AppleLoginButton {
+        print("Apple")
+    }
+}
