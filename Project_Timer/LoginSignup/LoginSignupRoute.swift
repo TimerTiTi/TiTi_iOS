12934981bd4b22ae2f2b9ca7746dec99f5abab9c
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-09-24 18:13:44 +0900

    feat #91: navigationPath 추가 및 NavigationStack 기본구조 작성

---
+++ Project_Timer/LoginSignup/LoginSignupRoute.swift
@@ -0,0 +1,14 @@
+//
+//  LoginSignupRoute.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/09/24.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import Foundation
+
+enum LoginSignupRoute {
+    case nickname
+    case login
+}
---
+++ Project_Timer/LoginSignup/SignupNicknameView.swift
@@ -0,0 +1,25 @@
+//
+//  SignupNicknameView.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/09/24.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignupNicknameView: View {
+    @Binding var navigationPath: [LoginSignupRoute]
+    
+    var body: some View {
+        Text("SignupNicknameView")
+    }
+}
+
+struct SignupNicknameView_Previews: PreviewProvider {
+    @State static private var navigationPath: [LoginSignupRoute] = []
+    
+    static var previews: some View {
+        SignupNicknameView(navigationPath: $navigationPath)
+    }
+}
