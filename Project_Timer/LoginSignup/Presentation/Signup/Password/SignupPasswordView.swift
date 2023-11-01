55061152d414288f44c858ea2497832ecf7b7c0e
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-01 15:44:59 +0900

    fix #114: scrollView 터치영역 확장, 키보드가림 문제 해결

--- Project_Timer/LoginSignup/Presentation/Signup/Password/SignupPasswordView.swift
+++ Project_Timer/LoginSignup/Presentation/Signup/Password/SignupPasswordView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct SignupPasswordView: View {
-    @EnvironmentObject var environment: LoginSignupEnvironment
+    @ObservedObject private var keyboard = KeyboardResponder.shared
     @StateObject private var model: SignupPasswordModel
     
     init(model: SignupPasswordModel) {
@@ -23,6 +23,7 @@ struct SignupPasswordView: View {
                     .ignoresSafeArea()
                 
                 ContentView(model: model)
+                    .padding(.bottom, keyboard.keyboardHeight+16)
             }
             .onChange(of: geometry.size, perform: { value in
                 model.updateContentWidth(size: value)
@@ -46,55 +47,66 @@ struct SignupPasswordView: View {
             ZStack {
                 ScrollViewReader { scrollViewProxy in
                     ScrollView {
-                        VStack(alignment: .leading, spacing: 0) {
-                            SignupTitleView(title: "비밀번호를 입력해주세요", subTitle: "8자리 이상 비밀번호를 입력해주세요")
-                            
-                            SignupSecureFieldView(type: .password, keyboardType: .alphabet, text: $model.password, focus: $focus) {
-                                model.checkPassword()
+                        HStack {
+                            Spacer()
+                            VStack(alignment: .leading, spacing: 0) {
+                                SignupTitleView(title: "비밀번호를 입력해주세요", subTitle: "8자리 이상 비밀번호를 입력해주세요")
+                                
+                                SignupSecureFieldView(type: .password, keyboardType: .alphabet, text: $model.password, focus: $focus) {
+                                    model.checkPassword()
+                                }
+                                SignupTextFieldUnderlineView(color: model.passwordTintColor)
+                                SignupTextFieldWarning(warning: "영문, 숫자, 또는 10가지 특수문자 내에서 입력해 주세요", visible: model.validPassword == false && model.password.isEmpty)
+                                    .id(SignupTextFieldView.type.password)
+                                
+                                if model.stage == .password2 {
+                                    NextContentView(focus: $focus, model: model, scrollViewProxy: scrollViewProxy)
+                                }
                             }
-                            .id(SignupTextFieldView.type.password)
-                            SignupTextFieldUnderlineView(color: model.passwordTintColor)
-                            SignupTextFieldWarning(warning: "영문, 숫자, 또는 10가지 특수문자 내에서 입력해 주세요", visible: model.validPassword == false)
-                            
-                            if model.stage == .password2 {
-                                NextContentView(focus: $focus, model: model)
+                            .onAppear {
+                                if model.stage == .password {
+                                    focus = .password
+                                }
                             }
-                        }
-                        .onAppear {
-                            if model.stage == .password {
-                                focus = .password
+                            .onChange(of: focus) { newValue in
+                                model.updateFocus(to: newValue)
+                                scroll(scrollViewProxy, to: newValue)
                             }
-                        }
-                        .onChange(of: focus) { newValue in
-                            model.updateFocus(to: newValue)
-                            #if targetEnvironment(macCatalyst)
-                            #else
-                            scrollViewProxy.scrollTo(newValue, anchor: .top)
-                            #endif
-                        }
-                        .onReceive(model.$stage) { status in
-                            switch status {
-                            case .password:
-                                focus = .password
-                            case .password2:
-                                focus = .password2
+                            .onReceive(model.$stage) { status in
+                                switch status {
+                                case .password:
+                                    focus = .password
+                                case .password2:
+                                    focus = .password2
+                                }
+                                scroll(scrollViewProxy, to: focus)
                             }
-                        }
-                        .onReceive(model.$validPassword2) { valid in
-                            if valid == true {
-                                environment.navigationPath.append(SignupPasswordRoute.signupNickname)
+                            .onReceive(model.$validPassword2) { valid in
+                                if valid == true {
+                                    environment.navigationPath.append(SignupPasswordRoute.signupNickname)
+                                }
                             }
+                            .frame(width: model.contentWidth)
+                            Spacer()
                         }
                     }
+                    .scrollIndicators(.hidden)
                 }
             }
-            .frame(width: model.contentWidth)
+        }
+        
+        func scroll(_ scrollViewProxy: ScrollViewProxy, to: any Hashable) {
+            #if targetEnvironment(macCatalyst)
+            #else
+            scrollViewProxy.scrollTo(to, anchor: .bottom)
+            #endif
         }
     }
     
     struct NextContentView: View {
         @FocusState.Binding var focus: SignupTextFieldView.type?
         @ObservedObject var model: SignupPasswordModel
+        let scrollViewProxy: ScrollViewProxy
         
         var body: some View {
             VStack(alignment: .leading, spacing: 0) {
@@ -109,9 +121,9 @@ struct SignupPasswordView: View {
                 SignupSecureFieldView(type: .password2, keyboardType: .alphabet, text: $model.password2, focus: $focus) {
                     model.checkPassword2()
                 }
-                .id(SignupTextFieldView.type.password2)
                 SignupTextFieldUnderlineView(color: model.password2TintColor)
-                SignupTextFieldWarning(warning: "동일하지 않습니다. 다시 입력해 주세요", visible: model.validPassword2 == false)
+                SignupTextFieldWarning(warning: "동일하지 않습니다. 다시 입력해 주세요", visible: model.validPassword2 == false && model.password2.isEmpty)
+                    .id(SignupTextFieldView.type.password2)
             }
         }
     }
