b114d5aeb566b721126c3b9247d9cf936517a727
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-21 15:26:14 +0900

    feat #117: SignupNicknameView, Model 구현

--- Project_Timer/Global/SingletonClass/PredicateChecker.swift
+++ Project_Timer/Global/SingletonClass/PredicateChecker.swift
@@ -13,6 +13,7 @@ struct PredicateChecker {
     
     static let emailCheck = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}" // email 형식
     static let passwordCheck = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\(allowedSpecialCharacters)]).*$" // 8자리 ~ 20자리 영어 + 숫자 + 허용된 특수문자
+    static let nicknameCheck = "[A-Z0-9a-z,./<>?;':!@#$%^&*()-=_+]{2,12}" // 닉네임 형식
     
     static func resultOfPredicate(text: String, cheker: String) -> Bool {
         let predicate = NSPredicate(format: "SELF MATCHES %@", cheker)
@@ -25,5 +26,9 @@ struct PredicateChecker {
     
     static func isValidPassword(_ password: String) -> Bool {
         return resultOfPredicate(text: password, cheker: passwordCheck) && password.count >= 8 && password.count <= 20
+    }
+    
+    static func isValidNickname(_ nickname: String) -> Bool {
+        return resultOfPredicate(text: nickname, cheker: nicknameCheck)
     }
 }
