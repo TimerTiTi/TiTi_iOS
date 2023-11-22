73a4c1e4a65a0f88fb86cf9a09fdf6c1b6ff74fe
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-22 12:19:56 +0900

    refac #119: CustomButton으로 AppleLogin 구현 및 ASAutorizationControllerDelegate 사용방식으로 변경

--- Project_Timer/LoginSignup/Presentation/Login/LoginSelect/AppleLoginButton.swift
+++ Project_Timer/LoginSignup/Presentation/Login/LoginSelect/AppleLoginButton.swift
@@ -7,65 +7,29 @@
 //
 
 import SwiftUI
-import AuthenticationServices
-
-enum AppleSigninError: Error {
-    case failure(String)
-    case noAuthorizationCode
-    case passwordCredential
-}
 
 struct AppleLoginButton: View {
-    var completion: (Result<(String, String?), AppleSigninError>) -> Void
+    var action: () -> Void
     
     var body: some View {
-        SignInWithAppleButton(.signIn) { request in
-            request.requestedScopes = [.fullName, .email]
-        } onCompletion: { result in
-            switch result {
-            case let .success(authorization):
-                switch authorization.credential {
-                case let appleIDCredential as ASAuthorizationAppleIDCredential:
-                    if let authorizationCode = appleIDCredential.authorizationCode {
-                        completion(.success((String(data: authorizationCode, encoding: .utf8)!, appleIDCredential.email)))
-                    } else {
-                        completion(.failure(.noAuthorizationCode))
-                    }
-                case _ as ASPasswordCredential:
-                    completion(.failure(.passwordCredential))
-                default:
-                    completion(.failure(.failure("failure")))
+        Button {
+            action()
+        } label: {
+            ZStack{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                    .shadow(color: .gray.opacity(0.1), radius: 4, x: 1, y: 2)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 58)
+                
+                HStack(spacing: 12) {
+                    Text("")
+                        .font(.system(size: 30))
+                    Text("Sign in with \("Apple")")
+                        .font(.system(size: 20, weight: .bold, design: .default))
                 }
-            case let .failure(error):
-                completion(.failure(.failure("\(error.localizedDescription)")))
             }
         }
-        .signInWithAppleButtonStyle(.white)
-        .font(.system(size: 20, weight: .bold, design: .default))
-        .cornerRadius(12)
-        .shadow(color: .gray.opacity(0.1), radius: 4, x: 1, y: 2)
-        .frame(maxWidth: .infinity)
-        .frame(height: 58)
-        
-
-//        Button {
-//            action()
-//        } label: {
-//            ZStack{
-//                RoundedRectangle(cornerRadius: 12)
-//                    .foregroundColor(.white)
-//                    .shadow(color: .gray.opacity(0.1), radius: 4, x: 1, y: 2)
-//                    .frame(maxWidth: .infinity)
-//                    .frame(height: 58)
-//                
-//                HStack(spacing: 12) {
-//                    Text("")
-//                        .font(.system(size: 30))
-//                    Text("Sign in with \("Apple")")
-//                        .font(.system(size: 20, weight: .bold, design: .default))
-//                }
-//            }
-//        }
-//        .foregroundColor(.black)
+        .foregroundColor(.black)
     }
 }
--- Project_Timer/LoginSignup/Presentation/Login/LoginSelect/LoginSelectModel.swift
+++ Project_Timer/LoginSignup/Presentation/Login/LoginSelect/LoginSelectModel.swift
@@ -7,13 +7,15 @@
 //
 
 import Foundation
+import AuthenticationServices
 
-class LoginSelectModel: ObservableObject {
+class LoginSelectModel: ObservableObject & NSObject {
     @Published var contentWidth: CGFloat = .zero
     @Published var venderInfo: SignupVenderInfo?
     @Published var showAleret: Bool = false
     @Published var errorMessage: (title: String, text: String) = ("", "")
     private(set) var vender: SignupVenderInfo.vender?
+    private(set) var authorizationCode: String?
 }
 
 // MARK: Action
@@ -27,26 +29,6 @@ extension LoginSelectModel {
             contentWidth = abs(size.minLength - 48)
         default:
             contentWidth = 400
-        }
-    }
-    
-    func appleLogin(_ result: Result<(String, String?), AppleSigninError>) {
-        self.vender = .apple
-        switch result {
-        case .success(let infos):
-            let authorizationCode = infos.0
-            let email = infos.1
-            // MARK: server 전송 필요
-            print(authorizationCode)
-            self.venderInfo = SignupVenderInfo(
-                vender: .apple,
-                id: "abcd1234",
-                email: email ?? ""
-            )
-            print(email)
-        case .failure(_):
-            self.errorMessage = (title: "Apple Login Fail", text: "Please terminate the app and try again.")
-            self.showAleret = true
         }
     }
 
@@ -65,5 +47,54 @@ extension LoginSelectModel {
             type: self.venderInfo?.email != nil ? .vender : .venderWithEmail,
             venderInfo: self.venderInfo
         )
+    }
+}
+
+extension LoginSelectModel {
+    private func postLogin(vender: SignupVenderInfo.vender, authorizationCode: String, email: String?) {
+        // MARK: server 전송 및 수신 필요
+        // MARK: 회원가입 & 로그인 분기처리 필요
+        let id = "abcd1234"
+        let email = email ?? ""
+        print(authorizationCode, email)
+        
+        self.venderInfo = SignupVenderInfo(
+            vender: vender,
+            id: id,
+            email: email
+        )
+    }
+}
+
+// MARK: AppleLogin
+extension LoginSelectModel: ASAuthorizationControllerDelegate {
+    func performAppleSignIn() {
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.performRequests()
+    }
+    
+    func authorizationController(controller: ASAuthorizationController,
+                                 didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            if let authorizationCode = appleIDCredential.authorizationCode {
+                self.postLogin(
+                    vender: .apple,
+                    authorizationCode: String(data: authorizationCode, encoding: .utf8)!,
+                    email: appleIDCredential.email
+                )
+            }
+        } else {
+            self.errorMessage = (title: "Apple Login Fail", text: "Please terminate the app and try again.")
+            self.showAleret = true
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        self.errorMessage = (title: "Apple Login Fail", text: "Please terminate the app and try again.")
+        self.showAleret = true
     }
 }
