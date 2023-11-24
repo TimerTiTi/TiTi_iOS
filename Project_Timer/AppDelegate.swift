72073be9ceed51b8df95c224234ce1f8852f500a
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-11-24 11:54:26 +0900

    feat #121: 인증 리디렉션 URL, 로그인 상태 복원 시도 반영

--- Project_Timer/AppDelegate.swift
+++ Project_Timer/AppDelegate.swift
@@ -11,6 +11,7 @@ import Firebase
 import FirebaseAnalytics
 import GoogleMobileAds
 import WidgetKit
+import GoogleSignIn
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,6 +24,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.checkLatestVersion()
+        self.checkGoogleSignOut()
         
         if Infos.isDevMode == false {
             self.configureGoogleAdmob()
@@ -68,6 +70,21 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
             return .all
         }
     }
+    
+    // MARK: Redirection
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
+        
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        
+        // Handle other custom URL types.
+        
+        // If not handled by this app, return false.
+        return false
+    }
 }
 
 /// Foreground 모드에서 notification 알림을 설정하기 위한 부분
@@ -106,6 +123,19 @@ extension AppDelegate {
                 }
             case .failure(let error):
                 print(error.alertMessage)
+            }
+        }
+    }
+    
+    private func checkGoogleSignOut() {
+        /// 사용자의 로그인 상태 복원 시도
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+                // MARK: SignOut 구현
+                print("Show the app's signed-out state")
+            } else {
+                // MARK: SignIn 관련 로직 필요한지 확인 필요
+                print("Show the app's signed-in state")
             }
         }
     }
