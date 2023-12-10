9a7f949e67d171b769d709954a5029462d495484
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-12-11 00:01:47 +0900

    feat #126: TLRko, TLRen, TLRzh 생성

---
+++ Project_Timer/Core/Localize/TLRen.swift
@@ -0,0 +1,16 @@
+//
+//  TLRen.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/12/10.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import Foundation
+
+enum TLRen: String {
+    case Update_Popup_HardUpdateTitle = "You have to update the app"
+    case Update_Popup_HardUpdateDesc = "Please update the latest version"
+    case Update_Pupup_SoftUpdateTitle = "New Version"
+    case Update_Popup_SoftUpdateDesc = "Try {} version"
+}
---
+++ Project_Timer/Core/Localize/TLRzh.swift
@@ -0,0 +1,16 @@
+//
+//  TLRzh.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2023/12/10.
+//  Copyright © 2023 FDEE. All rights reserved.
+//
+
+import Foundation
+
+enum TLRzh: String {
+    case Update_Popup_HardUpdateTitle = "需要更新"
+    case Update_Popup_HardUpdateDesc = "请更新到最新版本"
+    case Update_Pupup_SoftUpdateTitle = "新版本发布"
+    case Update_Popup_SoftUpdateDesc = "{} 请使用版本"
+}
