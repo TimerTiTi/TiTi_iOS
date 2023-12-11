58c59615ccd7d6eff6b3412f24ebd5b4cfffbb09
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-12-11 14:58:38 +0900

    feat #126: Typographys.uifont 적용 구현

--- Project_Timer/Core/Design System/Theme/Typographys.swift
+++ Project_Timer/Core/Design System/Theme/Typographys.swift
@@ -30,6 +30,17 @@ struct Typographys {
         case .zh: return MiSans(key, size)
         }
     }
+    
+    static func uifont(_ key: Key, size: CGFloat) -> UIFont? {
+        // MARK: 추후 UserDefaults 값 추가확인 필요
+        let language = Language.system
+        
+        switch language {
+        case .ko: return HGGGOTHICSSIP_uifont(key, size)
+        case .en: return HGGGOTHICSSIP_uifont(key, size)
+        case .zh: return MiSans_uifont(key, size)
+        }
+    }
 }
 
 extension Typographys {
@@ -53,6 +64,44 @@ extension Typographys {
     
     // MARK: zh
     static func MiSans(_ key: Key, _ size: CGFloat) -> Font {
+        switch key {
+        case .extraLight:
+            return Fonts.MiSansExtraLight(size: size)
+        case .light:
+            return Fonts.MiSansLight(size: size)
+        case .normal:
+            return Fonts.MiSansNormal(size: size)
+        case .semibold:
+            return Fonts.MiSansMedium(size: size)
+        case .bold:
+            return Fonts.MiSansSemibold(size: size)
+        case .heavy:
+            return Fonts.MiSansBold(size: size)
+        }
+    }
+}
+
+extension Typographys {
+    // MARK: ko, en
+    static func HGGGOTHICSSIP_uifont(_ key: Key, _ size: CGFloat) -> UIFont? {
+        switch key {
+        case .extraLight:
+            return Fonts.HGGGothicssiP00g(size: size)
+        case .light:
+            return Fonts.HGGGothicssiP20g(size: size)
+        case .normal:
+            return Fonts.HGGGothicssiP40g(size: size)
+        case .semibold:
+            return Fonts.HGGGothicssiP60g(size: size)
+        case .bold:
+            return Fonts.HGGGothicssiP80g(size: size)
+        case .heavy:
+            return Fonts.HGGGothicssiP99g(size: size)
+        }
+    }
+    
+    // MARK: zh
+    static func MiSans_uifont(_ key: Key, _ size: CGFloat) -> UIFont? {
         switch key {
         case .extraLight:
             return Fonts.MiSansExtraLight(size: size)
