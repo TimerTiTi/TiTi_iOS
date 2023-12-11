52c9c1358d50fb012b9dfc811a9899feba1ccda9
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-12-11 14:44:02 +0900

    feat #126: Typographys 생성

--- Project_Timer/Core/Design System/Theme/Typographys.swift
+++ Project_Timer/Core/Design System/Theme/Typographys.swift
@@ -10,3 +10,62 @@ import Foundation
 import SwiftUI
 import UIKit
 
+struct Typographys {
+    enum Key {
+        case extraLight
+        case light
+        case normal
+        case semibold
+        case bold
+        case heavy
+    }
+    
+    static func font(_ key: Key, size: CGFloat) -> Font {
+        // MARK: 추후 UserDefaults 값 추가확인 필요
+        let language = Language.system
+        
+        switch language {
+        case .ko: return HGGGOTHICSSIP(key, size)
+        case .en: return HGGGOTHICSSIP(key, size)
+        case .zh: return MiSans(key, size)
+        }
+    }
+}
+
+extension Typographys {
+    // MARK: ko, en
+    static func HGGGOTHICSSIP(_ key: Key, _ size: CGFloat) -> Font {
+        switch key {
+        case .extraLight:
+            return Fonts.HGGGothicssiP00g(size: size)
+        case .light:
+            return Fonts.HGGGothicssiP20g(size: size)
+        case .normal:
+            return Fonts.HGGGothicssiP40g(size: size)
+        case .semibold:
+            return Fonts.HGGGothicssiP60g(size: size)
+        case .bold:
+            return Fonts.HGGGothicssiP80g(size: size)
+        case .heavy:
+            return Fonts.HGGGothicssiP99g(size: size)
+        }
+    }
+    
+    // MARK: zh
+    static func MiSans(_ key: Key, _ size: CGFloat) -> Font {
+        switch key {
+        case .extraLight:
+            return Fonts.MiSansExtraLight(size: size)
+        case .light:
+            return Fonts.MiSansLight(size: size)
+        case .normal:
+            return Fonts.MiSansNormal(size: size)
+        case .semibold:
+            return Fonts.MiSansMedium(size: size)
+        case .bold:
+            return Fonts.MiSansSemibold(size: size)
+        case .heavy:
+            return Fonts.MiSansBold(size: size)
+        }
+    }
+}
