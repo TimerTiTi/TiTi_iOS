77e59ea030df675a93036ead69febd0a6d942a35
FreeDeveloper97 <freedeveloper97@gmail.com>
2024-03-16 10:19:56 +0900

    feat #137: ResetPasswordVC 화면전환

---
+++ Project_Timer/Present/Setting/Service/SettingTiTiLab/TestServer/ResetPassword/ResetPasswordNicknameView.swift
@@ -0,0 +1,19 @@
+//
+//  ResetPasswordNicknameView.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2024/03/16.
+//  Copyright © 2024 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResetPasswordNicknameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResetPasswordNicknameView()
+}
---
+++ Project_Timer/Present/Setting/Service/SettingTiTiLab/TestServer/ResetPassword/ResetPasswordVC.swift
@@ -0,0 +1,64 @@
+//
+//  ResetPasswordVC.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2024/03/16.
+//  Copyright © 2024 FDEE. All rights reserved.
+//
+
+import UIKit
+import Combine
+import SwiftUI
+
+final class ResetPasswordVC: PortraitVC {
+    private var environment: ResetPasswordEnvironment?
+    private var cancellables: Set<AnyCancellable> = []
+    
+    override func loadView() {
+        super.loadView()
+        self.environment = ResetPasswordEnvironment(rootVC: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+        
+        self.configureHostingVC()
+        self.bindListener()
+    }
+    
+    private func configureHostingVC() {
+        guard let environment = self.environment else { return }
+        let hostingVC = UIHostingController(rootView: ResetPasswordNicknameView().environmentObject(environment))
+        self.addChild(hostingVC)
+        hostingVC.didMove(toParent: self)
+        
+        hostingVC.view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(hostingVC.view)
+        NSLayoutConstraint.activate([
+            hostingVC.view.topAnchor.constraint(equalTo: self.view.topAnchor),
+            hostingVC.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            hostingVC.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            hostingVC.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+    }
+    
+    private func bindListener() {
+        self.environment?.$dismiss
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] dismiss in
+                if dismiss {
+                    self?.dismiss(animated: true)
+                }
+            }
+            .store(in: &self.cancellables)
+        
+        self.environment?.$resetSuccess
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] success in
+                guard success else { return }
+                self?.dismiss(animated: true)
+            }
+            .store(in: &self.cancellables)
+    }
+}
