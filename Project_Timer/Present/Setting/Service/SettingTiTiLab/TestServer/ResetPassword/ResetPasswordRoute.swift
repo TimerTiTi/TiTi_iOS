747b62963416513a78dad743432b111ed6fd2f58
FreeDeveloper97 <freedeveloper97@gmail.com>
2024-04-27 16:35:05 +0900

    feat #138 : ResetPasswordRoute 파일 추가, 7.17.2(2) 배포

---
+++ Project_Timer/Present/Setting/Service/SettingTiTiLab/TestServer/ResetPassword/ResetPasswordRoute.swift
@@ -0,0 +1,12 @@
+//
+//  ResetPasswordRoute.swift
+//  Project_Timer
+//
+//  Created by Kang Minsang on 2024/03/16.
+//  Copyright © 2024 FDEE. All rights reserved.
+//
+import Foundation
+
+enum ResetPasswordRoute {
+    case resetPasswordComplete
+}
