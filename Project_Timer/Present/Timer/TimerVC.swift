8a56520ae3dce38479d5bca934568301ff60c767
sladuf <04fud27@gmail.com>
2024-06-27 12:32:01 +0900

    #152 feat: 목표시간 수정 팝업 불필요한 띄어쓰기 삭제

--- Project_Timer/Present/Timer/TimerVC.swift
+++ Project_Timer/Present/Timer/TimerVC.swift
@@ -826,7 +826,7 @@ extension TimerVC {
     private func showSettingTargetTime() {
         guard let targetTimeSettingVC = storyboard?.instantiateViewController(withIdentifier: TargetTimeSettingPopupVC.identifier) as? TargetTimeSettingPopupVC else { return }
         let info = TargetTimeSettingInfo(title: Localized.string(.Recording_Text_EditTargetTimeTitle),
-                                         subTitle: "\(Date().YYYYMMDDstyleString) " + Localized.string(.Recording_Text_EditTargetTimeSubtitle),
+                                         subTitle: Date().YYYYMMDDstyleString + Localized.string(.Recording_Text_EditTargetTimeSubtitle),
                                          targetTime: RecordsManager.shared.recordTimes.settedGoalTime)
         targetTimeSettingVC.configure(info: info)
         
