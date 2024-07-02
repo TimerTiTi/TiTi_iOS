bc259625c31ca3b65c34915f05202b743fe7c119
sladuf <04fud27@gmail.com>
2024-07-02 19:30:17 +0900

    #159 feat: SwiftUI Toast 구현

---
+++ Project_Timer/Present/ToastMessage/ToastView.swift
@@ -0,0 +1,82 @@
+//
+//  ToastView.swift
+//  Project_Timer
+//
+//  Created by Ryeong on 6/24/24.
+//  Copyright © 2024 FDEE. All rights reserved.
+//
+
+import SwiftUI
+
+enum ToastType {
+    case newRecord
+}
+
+protocol ToastViewProtocol: View {
+    var presenter: ToastPresenter { get }
+}
+
+struct ToastView<Content: View>: ToastViewProtocol {
+    @ObservedObject var presenter: ToastPresenter
+    @State private var yOffset: CGFloat = 0
+    let content: (_ presenter: ToastPresenter) -> Content
+    
+    var body: some View {
+        ZStack {
+            content(presenter)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 160)
+                .frame(height: 46)
+                .foregroundColor(Color.white)
+                .shadow(color: .gray.opacity(0.12),
+                        radius: 4, x: 0, y: 4)
+        )
+        .offset(y: yOffset)
+        .onAppear(perform: {
+            show()
+        })
+    }
+    
+    private func show() {
+        if #available(iOS 17.0, *) {
+            withAnimation(.easeIn(duration: 0.15)) {
+                yOffset = 40
+            } completion: {
+                DispatchQueue.main.asyncAfter(deadline: .now()+1, execute: {
+                    hide()
+                })
+            }
+        } else {
+            withAnimation(.easeIn(duration: 0.15)) {
+                yOffset = 40
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now()+1.15, execute: {
+                hide()
+            })
+        }
+    }
+    
+    private func hide() {
+        if #available(iOS 17.0, *) {
+            withAnimation(.easeOut(duration: 0.15)) {
+                yOffset = 0
+            } completion: {
+                presenter.isPresenting = false
+            }
+        } else {
+            withAnimation(.easeOut(duration: 0.15)) {
+                yOffset = 0
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.15, execute: {
+                presenter.isPresenting = false
+            })
+        }
+    }
+}
+
+#Preview {
+    ToastView(presenter: ToastPresenter()) { presenter in
+        newRecordToastView(presenter: presenter)
+    }
+}
